2b478aa1f1b4a9defff3be19f8997bc3a64f7669
Michael Agra <michael.agra@yarilabs.com>
2020-05-04 10:04:52 +0100

    using Jason

--- lib/one_signal/api.ex
+++ lib/one_signal/api.ex
@@ -1,38 +1,38 @@
 defmodule OneSignal.API do
-
   def get(url, query \\ []) do
-    HTTPoison.start
+    HTTPoison.start()
     query = OneSignal.Utils.encode_body(query)
 
     unless String.length(query) == 0 do
       url = "#{url}?#{query}"
     end
 
-    HTTPoison.get!(url, OneSignal.auth_header)
+    HTTPoison.get!(url, OneSignal.auth_header())
     |> handle_response
   end
 
   def post(url, body) do
-    HTTPoison.start
+    HTTPoison.start()
 
-    req_body = Poison.encode!(body)
+    req_body = Jason.encode!(body)
 
-    HTTPoison.post!(url, req_body, OneSignal.auth_header)
+    HTTPoison.post!(url, req_body, OneSignal.auth_header())
     |> handle_response
   end
 
   def delete(url) do
-    HTTPoison.start
-    HTTPoison.delete!(url, OneSignal.auth_header)
+    HTTPoison.start()
+
+    HTTPoison.delete!(url, OneSignal.auth_header())
     |> handle_response
   end
 
-  defp handle_response(%HTTPoison.Response{body: body, status_code: code}) when code in 200..299 do
-    {:ok, Poison.decode!(body)}
+  defp handle_response(%HTTPoison.Response{body: body, status_code: code})
+       when code in 200..299 do
+    {:ok, Jason.decode!(body)}
   end
 
   defp handle_response(%HTTPoison.Response{body: body, status_code: _}) do
-    {:error, Poison.decode!(body)}
+    {:error, Jason.decode!(body)}
   end
-
 end
